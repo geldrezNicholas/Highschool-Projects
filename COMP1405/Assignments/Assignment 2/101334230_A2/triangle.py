3801e66b157e0959677dda35dd295b81dcdbb155
geldrezNicholas <geldrez.nicholas@gmail.com>
2025-01-25 22:02:53 -0500

    Delete COMP1405 directory

--- COMP1405/Assignments/Assignment 2/101334230_A2/triangle.py
+++
@@ -1,8 +0,0 @@
-a = int(input('1:'))
-b = int(input('2:'))
-c = int(input('3:'))
-
-if((a+b > c) and (b+c>a) and (a+c>b)):
-    print('This is a triangle.')
-else:
-    print('This is not a triangle.')
